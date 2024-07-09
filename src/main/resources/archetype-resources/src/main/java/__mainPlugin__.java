808f76843d3a238bfb650a4a153e6124dfcb8092
hsgamer <huynhqtienvtag@gmail.com>
2024-07-09 23:26:06 +0700

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/__mainPlugin__.java
@@ -0,0 +1,16 @@
+package $package;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public final class $mainPlugin extends SimplePlugin {
+
+    @Override
+    protected void onPluginStart() {
+
+    }
+
+    @Override
+    protected void onPluginStop() {
+
+    }
+}
